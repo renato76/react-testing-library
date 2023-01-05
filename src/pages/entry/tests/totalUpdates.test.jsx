25795092fd19f9249900fd0cde99e81edd604c67
Renato Mignogna <renmignogna@gmail.com>
2023-01-05 21:10:39 +0000

    create order details context and provider

--- src/pages/entry/tests/totalUpdates.test.jsx
+++ src/pages/entry/tests/totalUpdates.test.jsx
@@ -8,15 +8,21 @@ test('update scoop total when scoops change', async () => {
 
   // make sure total starts out at $0.00
   const scoopsSubtotal = screen.getByText('Scoops total: $', { exact: false })
-  expect(scoopsSubtotal).toHaveTextContent('0.00')
+  expect(scoopsSubtotal).toHaveTextContent("0.00")
 
   // update vanilla scoops to 1 and check subtotal
-  const vanillaInput = await screen.findByRole('spinbutton', { 
-    name: 'Vanilla', 
+  const vanillaInput = await screen.findByRole("spinbutton", { 
+    name: "Vanilla", 
   })
   await user.clear(vanillaInput)
   await user.type(vanillaInput, "1")
   expect(scoopsSubtotal).toHaveTextContent("2.00")
 
   // update chocolate scops to 2 and check subtotal 
+  const chocolateInput = await screen.findByRole("spinbutton", {
+    name: "Chocolate",
+  })
+  await user.clear(chocolateInput)
+  await user.type(chocolateInput, "2")
+  expect(scoopsSubtotal).toHaveTextContent("6.00")
 })
